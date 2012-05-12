ab2038348065874301a49d8a8f78093024853373
becyn <yucalcium.c6@gmail.com>
2012-05-12 15:03:49 +0900

    touched config/initializers/omniauth.rb.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,4 @@
+Rails.application.config.middleware.use OmniAuth::Builder do                                                                                                                                                                  
+  config = Rails.application.config.twitter["development"]                                                                                                                                                                    
+  provider :twitter, config["consumer_key"], config["consumer_secret"]                                                                                                                                                        
+end
