3f7196b3a8eb4a0cac9a44a3b17dc02263a0ee2a
becyn <yucalcium.c6@gmail.com>
2012-05-12 16:09:06 +0900

    edited routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Cont24::Application.routes.draw do
   resources :anniversaries
+  resources :users, :only => [:show]
   root :to => 'Pages#index'
 
   get "/auth/:provider/callback" => "sessions#callback"
