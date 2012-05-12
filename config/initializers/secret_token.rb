9fee50e1a91c64abcd20ae5c51166a140bd86521
asonas <hzw1258@gmail.com>
2012-05-12 13:52:27 +0900

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cont24::Application.config.secret_token = '2b73f8736c9cf469672e4fded5be8a78e559b937aef3ac2a674100b00712076fe820df6b396ecab2fc33d3f04c7123459dab50a0a6a59e40fb3dfafe09d9bafb'
