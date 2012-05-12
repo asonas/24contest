97148821f1228e2b6424f1ec0fd4eda0393517cd
becyn <yucalcium.c6@gmail.com>
2012-05-13 05:34:23 +0900

    ichibu shiyouu wo henkou suru.

--- app/controllers/anniversaries_controller.rb
+++ app/controllers/anniversaries_controller.rb
@@ -57,7 +57,7 @@ class AnniversariesController < ApplicationController
     end
     # FIXME 3rd parameter
     # tweet_body = "@%s は最高の幸せを噛み締めた。彼の1日は終わったのだ。 http://hoge.com/%s/%s" [@anniversary.user.twitter_id, @anniversary.user.twitter_id, @anniversary.id]
-    tweet_body = "@ #{@anniversary.user.twitter_id} は最高の幸せを噛み締めた。彼の1日は終わったのだ。"
+    tweet_body = "@ #{@anniversary.user.twitter_id} は最高の幸せを噛み締めた。そう、1日は終わったのだ。"
     tweet_result = current_user.tweet({:tweet_body => tweet_body, :user => current_user})
     @anniversary.tweet_id = tweet_result[:id].to_i
 
