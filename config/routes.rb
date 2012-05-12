ba46d3de3cd40154fb3d1d9eed2c39c1aff47aa4
asonas <hzw1258@gmail.com>
2012-05-12 15:54:21 +0900

    root追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Cont24::Application.routes.draw do
   resources :anniversaries
+  root :to => 'Pages#index'
 
   get "/auth/:provider/callback" => "sessions#callback"
   get "/logout" => "sessions#destroy", :as => :logout
