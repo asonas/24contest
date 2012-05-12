0f9b23e1341f2e5fedd575637f9af69b80dc5717
becyn <yucalcium.c6@gmail.com>
2012-05-12 16:09:26 +0900

    added new function authenticated? current_user authentication_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def authenticated?
+    return false if session[:user_id].nil? or session[:user_created_at].nil? or current_user.nil?
+    return true
+  end
+  helper_method :authenticated?
+
+  def current_user
+    @current_user ||= User.find_by_id_and_created_at(session[:user_id],session[:user_created_at])
+  end
+  helper_method :current_user
+  
+  def authentication_filter                                                                                                                             
+     return if authenticated?
+     redirect_to root_path
+  end
 end
